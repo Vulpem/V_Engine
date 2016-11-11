51f2b3b4e3401e348e8facbdea5e8ef8038b088b
Vulpem <davidhl2@tr12.cat>
2016-11-11 14:07:00 +0100

    Removed GL math

--- Engine/ModuleRenderer3D.h
+++ Engine/ModuleRenderer3D.h
@@ -3,7 +3,6 @@
 
 #include "Module.h"
 #include "Globals.h"
-#include "glmath.h"
 #include "Light.h"
 
 #define MAX_LIGHTS 8
@@ -51,8 +50,6 @@ public:
 	Light lights[MAX_LIGHTS];
 private:
 	SDL_GLContext context;
-	mat3x3 NormalMatrix;
-	mat4x4 ModelMatrix, ViewMatrix, ProjectionMatrix;
 };
 
 #endif
